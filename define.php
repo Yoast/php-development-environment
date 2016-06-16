d53b054d96b688a96e47d4c2e4306983375efd75
Jip Moors <jhp.moors@gmail.com>
2016-06-16 12:12:26 +0200

    Added defined check

--- define.php
+++ define.php
@@ -3,5 +3,9 @@
 /**
  * This define should not exist on production.
  * Because it has to be `install --dev` in composer it is only available in non-production environments.
+ * 
+ * Allow for overriding any other file loaded before this one.
  */
-define( 'YOAST_ENVIRONMENT', 'development' );
+if ( ! defined( 'YOAST_ENVIRONMENT' ) ) {
+	define( 'YOAST_ENVIRONMENT', 'development' );
+}
