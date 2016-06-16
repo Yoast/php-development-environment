d168fc71529bc717529e6ab112c3b822d69789b5
Jip Moors <jhp.moors@gmail.com>
2016-06-16 10:33:25 +0200

    Introduced define to use in development environments

---
+++ define.php
@@ -0,0 +1,7 @@
+<?php
+
+/**
+ * This define should not exist on production.
+ * Because it has to be `install --dev` in composer it is only available in non-production environments.
+ */
+define( 'YOAST_ENVIRONMENT', 'development' );
